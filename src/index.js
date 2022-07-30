a6a1f23d5b6d18ffc3f21ff534d7a120900daf6d
Shiv Bhonde <shivbhonde04@gmail.com>
2022-07-30 16:46:21 +0530

    add production uri

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import Pages from './pages';
 import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 
 const client = new ApolloClient({
-	uri: 'http://localhost:4000',
+	uri: 'https://spacecats-academy.herokuapp.com/',
 	cache: new InMemoryCache(),
 });
 
