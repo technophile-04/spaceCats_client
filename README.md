5e16dd2c948a3e65a613642aea5213f14d1494b3
Shiv Bhonde <shivbhonde04@gmail.com>
2022-07-30 15:59:46 +0530

    complete single module fetch

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # SpaceCats - client
 
-This the `frontend` which I created while learning Odyssey Lift-off course.
+This is the `frontend` which I created while learning Odyssey Lift-off course.
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,12 +3,14 @@ import { Router } from '@reach/router';
 import Track from './Track';
 /** importing our pages */
 import Tracks from './tracks';
+import Module from './Module';
 
 export default function Pages() {
 	return (
 		<Router primary={false} component={Fragment}>
 			<Tracks path="/" />
 			<Track path="/track/:trackId" />
+			<Module path="/track/:trackId/module/:moduleId" />
 		</Router>
 	);
 }
